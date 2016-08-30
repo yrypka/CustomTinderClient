3b15b512d17f8d3e377e5f70698f46dee6fbbeba
Yuriy Rypka <yurarypka@gmail.com>
2016-08-31 01:13:07 +0300

    Managing App State with Redux

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ReduxSimpleStarter
+# CustomTinderClient
 
 Interested in learning [Redux](https://www.udemy.com/react-redux/)?
 
@@ -10,16 +10,8 @@ There are two methods for getting started with this repo.
 Checkout this repo, install dependencies, then start the gulp process with the following:
 
 ```
-	> git clone git@github.com:StephenGrider/ReduxSimpleStarter.git
-	> cd ReduxSimpleStarter
-	> npm install
-	> npm start
-```
-
-####Not Familiar with Git?#####
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
-
-```
+	> git clone git@github.com:yrypka/CustomTinderClient.git
+	> cd CustomTinderClient
 	> npm install
 	> npm start
 ```
---
+++ src/containers/book-list.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import {selectBook} from '../actions';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const {books} = state;
+
+  return {
+    books
+  };
+}
+
+// Anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed
+  //  to all of our reducers
+  return bindActionCreators({
+    selectBook
+  }, dispatch);
+}
+
+// Promote BookList from a component to a container = it needs to know
+// about this new dispatch method, selectBook. Make it available
+// as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
